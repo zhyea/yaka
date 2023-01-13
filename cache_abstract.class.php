0e298b1a2de805b9eee88564c2991c3a82dbfa42
rui.zhang <rui.zhang@tendcloud.com>
2023-01-13 09:16:42 +0800

    init - 2 添加缓存抽象类
    Change-Id: I8d57dbd3605ee529fc6ec686051febc50b36aeb6

---
+++ cache_abstract.class.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+
+abstract class cache_abstract
+{
+
+    public $conf = array();
+
+    public $link = NULL;
+
+    public $prefix = '';
+
+    public $err_no = 0;
+
+    public $err_str = '';
+
+
+    public function connect()
+    {
+    }
+
+
+    abstract function set($k, $v, $life);
+
+
+    public function error($errno = 0, $err_str = '')
+    {
+        $this->err_no = $errno;
+        $this->err_str = $err_str;
+        DEBUG and trigger_error('Cache Error:' . $this->err_str);
+
+        return NULL;
+    }
+
+}
