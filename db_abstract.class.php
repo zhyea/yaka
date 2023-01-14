763e2cbfe9d36251e8915b6059efb6e967e4ac98
rui.zhang <rui.zhang@tendcloud.com>
2023-01-14 23:07:01 +0800

    init-3 : 开始调整数据库操作
    Change-Id: I91322a5411300b926143ec6fb72ff0f2a0e16867

---
+++ db_abstract.class.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+abstract class db_abstract
+{
+    /**
+     * @var array  配置，可以支持主从
+     */
+    public $conf = array();
+
+    /**
+     * @var array 配置，可以支持主从
+     */
+    public $conf_slave = array();
+
+    /**
+     * @var string 写连接
+     */
+    public $link_write = NULL;
+
+    /**
+     * @var string 读连接
+     */
+    public $link_read = NULL;
+
+    /**
+     * @var string 最后一次使用的连接
+     */
+    public $link = NULL;
+
+    public $err_no = 0;
+
+    public $err_str = '';
+
+    public $sql_array = array();
+
+    public $table_pre = '';
+}
